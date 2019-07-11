d322b4d744c1866ad0e3a12aa9248398377ac97e
Samarth Malhotra <malhotra.samarth@hotmail.com>
2019-07-11 15:01:35 +0530

    final commit

--- PS4_SR_G9/main.py
+++ PS4_SR_G9/main.py
@@ -4,30 +4,66 @@ StudentHashRecords = sh.StudentHashTable()
 StudentHashRecords.initializeHash()
 
 # Read student records from inputPS4.txt
-input_file = open("../inputPS4.txt")
-for record in input_file.readlines():
-    student = record.split(" / ")
-    studentId = student[0]
-    CGPA = float(student[1])
-    StudentHashRecords.insertStudentRec(studentId, CGPA)
+try:
+    input_file = open("../inputPS4.txt")
+    for record in input_file.readlines():
+        student = record.split(" / ")
+        studentId = student[0]
+        CGPA = float(student[1])
+        StudentHashRecords.insertStudentRec(studentId, CGPA)
+except FileNotFoundError as fe:
+    print(fe)
+except IOError as ioe:
+    print(ioe)
+finally:
+    input_file.close()
 
 # Print HallOfFame list
-prompt_file = open("../promptPS4.txt")
-for record in prompt_file.readlines():
-    if "hallOfFame" in record:
-        CGPA_hf = float(record.split(":")[1])
-StudentHashRecords.hallOfFame(CGPA_hf)
+try:
+    prompt_file = open("../promptPS4.txt")
+    for record in prompt_file.readlines():
+        if "hallOfFame" in record:
+            CGPA_hf = float(record.split(":")[1])
+    hall_of_fame = StudentHashRecords.hallOfFame(CGPA_hf)
+    output_file = open("../outputPS4.txt", "a+")
+    output_file.write(hall_of_fame)
+    output_file.close()
+except FileNotFoundError as fe:
+    print(fe)
+except IOError as ioe:
+    print(ioe)
+finally:
+    prompt_file.close()
+    output_file.close()
 
 # Print Course Offer list
-prompt_file = open("../promptPS4.txt")
-for record in prompt_file.readlines():
-    if "courseOffer" in record:
-        CGPAFrom = float(record.split(":")[1])
-        CGPATo = float(record.split(":")[2])
-StudentHashRecords.newCourseList(CGPAFrom, CGPATo)
-
+try:
+    prompt_file = open("../promptPS4.txt")
+    for record in prompt_file.readlines():
+        if "courseOffer" in record:
+            CGPAFrom = float(record.split(":")[1])
+            CGPATo = float(record.split(":")[2])
+    new_course_list = StudentHashRecords.newCourseList(CGPAFrom, CGPATo)
+    output_file = open("../outputPS4.txt", "a+")
+    output_file.write(new_course_list)
+    output_file.close()
+except FileNotFoundError as fe:
+    print(fe)
+except IOError as ioe:
+    print(ioe)
+finally:
+    prompt_file.close()
 
 # Print average CGPA department wise
-StudentHashRecords.depAvg()
+try:
+    output_file = open("../outputPS4.txt", "a+")
+    output_file.write(StudentHashRecords.depAvg())
+    output_file.close()
+except FileNotFoundError as fe:
+    print(fe)
+except IOError as ioe:
+    print(ioe)
+finally:
+    output_file.close()
 
 StudentHashRecords.destroyHash()
--- PS4_SR_G9/student_hash_table.py
+++ PS4_SR_G9/student_hash_table.py
@@ -26,7 +26,6 @@ class StudentHashTable:
     def insertStudentRec(StudentHashRecords, studentId, CGPA):
         next_slot = 0
         student = [studentId, CGPA]
-
         for count in range(StudentHashRecords.size):
             hash_id = StudentHashRecords.HashId(studentId, next_slot)
             if StudentHashRecords.students[hash_id] is None:
@@ -39,42 +38,34 @@ class StudentHashTable:
     # Method to create HallofFame output file
     def hallOfFame(StudentHashRecords, CGPA):
         qualified_students = []
-
         for student in StudentHashRecords.students:
             if (student is not None) and (student[1] > CGPA):
                 qualified_students.append(student)
-
-        output_file = open("../outputPS4.txt", "a+")
-        output_file.write("\n---------- hall of fame ----------")
-        output_file.write("\nInput: " + str(CGPA))
-        output_file.write("\nTotal eligible students: " +
-                          str(len(qualified_students)))
-        output_file.write("\nQualified students:")
+        hall_of_fame = "\n---------- hall of fame ----------"
+        hall_of_fame += "\nInput: " + str(CGPA)
+        hall_of_fame += "\nTotal eligible students: " + str(len(qualified_students))
+        hall_of_fame += "\nQualified students:"
         for student in qualified_students:
-            output_file.write("\n" + student[0] + " / " + str(student[1]))
-        output_file.write("\n-------------------------------------\n")
-        output_file.close()
+            hall_of_fame += "\n" + student[0] + " / " + str(student[1])
+        hall_of_fame += "\n-------------------------------------\n"
+        return hall_of_fame
 
     # Method to create the New Course List
     def newCourseList(StudentHashRecords, CGPAFrom, CGPATo):
         qualified_students = []
-
         for student in StudentHashRecords.students:
             if (student is not None) and (student[1] > CGPAFrom) and (student[1] < CGPATo):
                 qualified_students.append(student)
-
-        output_file = open("../outputPS4.txt", "a+")
-        output_file.write("\n---------- New Course Candidates ----------")
-        output_file.write("\nInput: " + str(CGPAFrom))
-        output_file.write(" to " + str(CGPATo))
-        output_file.write("\nTotal eligible students: " +
-                          str(len(qualified_students)))
-        output_file.write("\nQualified students:")
+        new_course_list = "\n---------- New Course Candidates ----------"
+        new_course_list += "\nInput: " + str(CGPAFrom) + " to " + str(CGPATo)
+        new_course_list += "\nTotal eligible students: " + str(len(qualified_students))
+        new_course_list += "\nQualified students:"
         for student in qualified_students:
-            output_file.write("\n" + student[0] + " / " + str(student[1]))
-        output_file.write("\n-------------------------------------\n")
-        output_file.close()
+            new_course_list += "\n" + student[0] + " / " + str(student[1])
+        new_course_list += "\n-------------------------------------\n"
+        return new_course_list
 
+    # Method to create department list with average and max
     def depAvg(StudentHashRecords):
         department_list = []
         for student in StudentHashRecords.students:
@@ -86,17 +77,15 @@ class StudentHashTable:
                 else:
                     department_list.append(department)
                     department_list.append([student[1]])
-        output_file = open("../outputPS4.txt", "a+")
-        output_file.write(
-            "\n---------- Department CGPA ----------")
+        dep_avg = "\n---------- Department CGPA ----------"
         for department in department_list:
             if isinstance(department, str):
                 list_index = department_list.index(department) + 1
                 marks = department_list[list_index]
-                output_file.write(
-                    f"\n{department},: max: {max(marks)}, avg: {sum(marks) / len(marks):.2f}")
-        output_file.write("\n-------------------------------------\n")
-        output_file.close()
+                dep_avg += f"\n{department},: max: {max(marks)}, avg: {sum(marks) / len(marks):.2f}"
+        dep_avg += "\n-------------------------------------\n"
+        return dep_avg
 
+    # Method to destroy the hash table
     def destroyHash(self):
         self.students = [None] * self.size
